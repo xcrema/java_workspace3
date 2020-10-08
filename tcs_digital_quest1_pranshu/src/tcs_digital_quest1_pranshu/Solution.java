082c5b7c35e5fa9bed1be7fb613f25e35d45f9c3
Achyut <achyut.terminator@gmail.com>
2020-10-08 11:00:26 +0530

    commit

---
+++ tcs_digital_quest1_pranshu/src/tcs_digital_quest1_pranshu/Solution.java
@@ -0,0 +1,24 @@
+package tcs_digital_quest1_pranshu;
+import java.util.*;
+
+public class Solution {
+	 public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int p1 = sc.nextInt();
+		int l1 = sc.nextInt();
+		int p2 = sc.nextInt();
+		int l2 = sc.nextInt();
+		int v1 = sc.nextInt();
+		int v2 = sc.nextInt();
+		int t = sc.nextInt();
+		if(v1 == v2) {
+			if(t%2 == 0) {
+				System.out.println("READ");
+			}
+		}
+		else {
+			
+		}
+		sc.close();
+	}
+}
